055a7c5cbd87a59fa112f452c24d29bab59fd670
Bonfra04 <bonfract@gmail.com>
2025-03-09 12:16:10 +0100

    exporting necessary files

--- lib/stronz_cast.dart
+++ lib/stronz_cast.dart
@@ -1,5 +1,2 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/stronz_cast_manager.dart';
+export 'src/stronz_caster_device.dart';
